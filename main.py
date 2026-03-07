0f307548038d283789bd821b565dde68ecd55fbf
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-03-08 01:23:06 +0600

    check out first fast api request

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-check=input("check is it working or not: ")
-print(check)
+from fastapi import FastAPI
+
+# create an instance of FastAPI
+app = FastAPI()
+
+# create a simple endpoint
+@app.get("/")
+def home():
+    return {"message": "FastAPI is running successfully!😒"}
+
+# create another endpoint
+@app.get("/hello/{name}")
+def greet(name: str):
+    return {"message": f"Hello {name}, welcome to FastAPI isolation learning!"}
